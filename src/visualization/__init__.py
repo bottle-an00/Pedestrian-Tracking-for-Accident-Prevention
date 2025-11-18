e769cf07d70a3eebc292eccef81ceb39049e0c15
jba <sjba10815@gmail.com>
2025-11-18 13:36:28 +0900

    feat(detector_integrator): integate(module) yolo tracker made by soooo

---
+++ src/visualization/__init__.py
@@ -0,0 +1,3 @@
+from .overlay_2d import Visualizer
+
+__all__ = ["Visualizer"]
--- src/visualization/overlay_2d.py
+++ src/visualization/overlay_2d.py
@@ -1,1 +1,33 @@
 # bbox, ID, 속도 overlay
+
+import cv2
+
+class Visualizer:
+    def draw_on_img(self, img, detections):
+        vis_img = img.copy()
+
+        for det in detections:
+            x, y, w, h = det['bbox']
+            x1, y1, x2, y2 = int(x), int(y), int(x + w), int(y + h)
+
+            track_id, cls_name, conf = det['id'], det['class'], det['score']
+
+            color = (255, 0, 0) if cls_name == "person" else (0, 255, 0)
+            label = f"ID:{track_id} {cls_name} {conf:.2f}"
+
+            cv2.rectangle(vis_img, (x1, y1), (x2, y2), color, 2)
+            cv2.putText(vis_img, label, (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+
+            foot_u, foot_v = map(int, det["foot_uv"])
+            cv2.circle(vis_img, (foot_u, foot_v), 5, (0, 0, 255), -1)
+
+        return vis_img
+
+    def draw_on_BEV(self, bev_img, foot_bevs):
+        vis_img = bev_img.copy()
+
+        for bx, by in foot_bevs:
+            cv2.circle(vis_img, (by, bx), 5, (0, 0, 255), -1)
+
+        return vis_img
